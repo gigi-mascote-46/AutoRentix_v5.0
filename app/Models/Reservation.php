92a5ca640c18fb489f2ca61d40256f8d91425154
gigi-mascote-46 <angelaisabelop@gmail.com>
2025-05-30 10:08:48 +0100

    refactor: Remove unused migration for adding is_admin column to users table

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -10,6 +10,11 @@ class Reservation extends Model
         'user_id', 'bem_locavel_id', 'data_inicio', 'data_fim', 'status'
     ];
 
+    protected $casts = [
+        'data_inicio' => 'datetime',
+        'data_fim' => 'datetime',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
